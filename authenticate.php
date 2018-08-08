0fedde488cd55ca5c63ba90d3b7a45f106f4ecbe
Nate Back <nkback@gmail.com>
2018-08-08 17:16:36 -0600

    Add login modal

--- authenticate.php
+++ authenticate.php
@@ -12,25 +12,28 @@
     $password = $_POST["password"];
     if(strrev($password) == $username){
         $_SESSION["random"] = "Unset";
-         $data = $conn->query("SELECT * FROM owners WHERE username='$username';");
-         if($data){
+         $data = $conn->query("SELECT * FROM owners WHERE username='admin' LIMIT 1");
+        $row = mysqli_fetch_assoc($data);
+        //$_SESSION["login"] = true;
+        $_SESSION["random"] = $_POST["username"];
+         //if($data){
             // if($data->num_rows != 0){
-                while($row = $data->fetch_assoc()){
-                    $_SESSION["login"] = true;
-                    $_SESSION["random"] = "Waht the hell";
+            //     while($row = $data->fetch_assoc()){
+            //         $_SESSION["login"] = true;
+            //         $_SESSION["random"] = "Waht the hell";
 
-            //         if($row["admin"]){
-            //             $_SESSION["admin"] = true;
-            //         }
-            //         else{
-            //             $_SESSION["admin"] = false;
-            //             $_SESSION["username"] = $row["id"];
-            //         }
-                }
+            // //         if($row["admin"]){
+            // //             $_SESSION["admin"] = true;
+            // //         }
+            // //         else{
+            // //             $_SESSION["admin"] = false;
+            // //             $_SESSION["username"] = $row["id"];
+            // //         }
+            //     }
             // }
             // else{
             // }
-        }
+        //}
     }
     // if(!isset($_SESSION["login"])){
     //     $_SESSION["error"] = true;
