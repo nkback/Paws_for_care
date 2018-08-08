ddf0550a1fbdda0dfbf253fa9dc29a7ab0df9532
root <root@LAPTOP-0ITO4NTL.localdomain>
2018-08-08 12:06:17 -0600

    Fix order by

--- notesData.php
+++ notesData.php
@@ -8,7 +8,7 @@
         die("Connection failed: ".$conn->connect_error);
     }
     // echo "Hello";
-    $data = $conn->query("select * from ".$_REQUEST['type']."Notes where ".$_REQUEST['type']."sFk=".$_REQUEST['id'].";");
+    $data = $conn->query("select * from ".$_REQUEST['type']."Notes where ".$_REQUEST['type']."sFk=".$_REQUEST['id']." ORDER BY date;");
     // $id = $data->fetch_assoc();
     // $newData = $conn->query("select * from owners where id=".$id["ownersFk"].";");
     echo json_encode($data->fetch_all());
