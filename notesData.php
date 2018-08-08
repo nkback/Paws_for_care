a7e5fb4069d8bf17c6f723bdf35d64fedd2a2cff
Nate Back <nkback@gmail.com>
2018-08-08 11:01:27 -0600

    Bug fix

--- notesData.php
+++ notesData.php
@@ -7,7 +7,7 @@
     if($conn->connect_error){
         die("Connection failed: ".$conn->connect_error);
     }
-    $data = $conn->query("select * from ".$_REQUEST['type']."Notes where ".$_REQUEST['type']."Fk=".$_REQUEST['id'].";");
+    $data = $conn->query("select * from ".$_REQUEST['type']."Notes where ".$_REQUEST['type']."sFk=".$_REQUEST['id'].";");
     // $id = $data->fetch_assoc();
     // $newData = $conn->query("select * from owners where id=".$id["ownersFk"].";");
     echo json_encode($newData->fetch_all());
