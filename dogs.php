0ec3233db812df8b2a80a39c850f04e61bdbb24f
Nate Back <nkback@gmail.com>
2018-08-08 13:30:42 -0600

    Change filter text

--- dogs.php
+++ dogs.php
@@ -260,9 +260,9 @@
                         <th scope="col" style="cursor: pointer" class="sortable" data-key="name">Name <span id="arrow"></span></th>
                         <th scope="col" style="cursor: pointer" class="sortable" data-key="breed">Breed <span id="arrow"></span></th>
                         <th scope="col" style="cursor: pointer" class="sortable" data-key="sex">Sex <span id="arrow"></span></th>
-                        <th scope="col" style="cursor: pointer" class="sortable" data-key="shots">Shots <span id="arrow"></span></th>
-                        <th scope="col" style="cursor: pointer" class="sortable" data-key="licensed">Licensed <span id="arrow"></span></th>
-                        <th scope="col" style="cursor: pointer" class="sortable" data-key="neutered">Neutered <span id="arrow"></span></th>
+                        <th scope="col" class="sortable" data-key="shots">Shots <span id="arrow"></span></th>
+                        <th scope="col" class="sortable" data-key="licensed">Licensed <span id="arrow"></span></th>
+                        <th scope="col" class="sortable" data-key="neutered">Neutered <span id="arrow"></span></th>
                         <th scope="col" style="cursor: pointer" class="sortable" data-key="age">Age <span id="arrow"></span></th>
                         <th scope="col" style="cursor: pointer" class="sortable" data-key="size">Weight <span id="arrow"></span></th>
                         <th scope="col" data-key="owners">Owners <span id="arrow"></span></th>
