171d4e15abf8f68165fcd0736dae89ccac12ad2f
Nate Back <nkback@gmail.com>
2018-08-08 18:31:58 -0600

    Add login modal

--- authenticate.php
+++ authenticate.php
@@ -11,35 +11,21 @@
     $username = $_POST["username"];
     $password = $_POST["password"];
     if(strrev($password) == $username){
-         $data = $conn->query("SELECT * FROM owners WHERE username='$username' LIMIT 1");
+        $data = $conn->query("SELECT * FROM owners WHERE username='$username' LIMIT 1");
         $row = mysqli_fetch_assoc($data);
-        if($row)
+
+        if($row) {
             $_SESSION["login"] = true;
-        else
-            $_SESSION["random"] = "Sad face";
-        //$_SESSION["random"] = $row["username"];
-         //if($data){
-            // if($data->num_rows != 0){
-            //     while($row = $data->fetch_assoc()){
-            //         $_SESSION["login"] = true;
-            //         $_SESSION["random"] = "Waht the hell";
-
-            // //         if($row["admin"]){
-            // //             $_SESSION["admin"] = true;
-            // //         }
-            // //         else{
-            // //             $_SESSION["admin"] = false;
-            // //             $_SESSION["username"] = $row["id"];
-            // //         }
-            //     }
-            // }
-            // else{
-            // }
-        //}
+            $_SESSION["admin"] = $row["admin"];
+            $_SESSION["userId"] = $row["id"];
+        }
+        else{
+            $_SESSION["error"] = "Username or password is incorrect.";
+        }
     }
-    // if(!isset($_SESSION["login"])){
-    //     $_SESSION["error"] = true;
-    // }
+    else
+        $_SESSION["error"] = "Username or password is incorrect.";
 
-    header('Location: '.$_SERVER['HTTP_REFERER']);
+
+    header("Location: " . $_SERVER["HTTP_REFERER"]);
 ?>
