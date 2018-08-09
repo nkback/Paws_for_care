e73714ca837d4d55de9d51bf5fbd2afb6409f7ae
Nate Back <nkback@gmail.com>
2018-08-08 19:01:55 -0600

    Add flash message

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <?php
     if(isset($_SESSION["error"])){
-        echo "<div class='alert alert-danger' style='margin-bottom: 0'>";
+        echo "<div class='alert alert-danger alert-dismissible fade show' style='margin-bottom: 0'>";
             echo $_SESSION["error"] . '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
         echo "</div>";
     }
